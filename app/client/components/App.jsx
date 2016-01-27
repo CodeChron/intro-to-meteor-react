31b701487bd4697652a73be35f6f9c39a5f8a616
Anders Ramsay <andersr@gmail.com>
2016-01-27 13:49:35 -0500

    remove login state from App component

--- app/client/components/App.jsx
+++ app/client/components/App.jsx
@@ -1,32 +1,9 @@
 App = React.createClass({
-  getDefaultProps() {
-    return {
-      signedIn: false
-    };
-  },
-  getInitialState() {
-    return {
-      signedIn: this.props.signedIn
-    };
-  },
   showUserNav(){
-    
-    let userNavLinks = [
-      {
-        label: "Sign Out",
-        path: "/logout"
-      }
-    ];
-
-    return this.state.signedIn?
-      //Placeholder for a Dropdown with User Info
-      <span>User Info</span>
-    :
-      <ul className="nav navbar-nav navbar-right">
-        <li><a href="/login">Login</a></li>
-        <li><a href="/register">Register</a></li>
-      </ul>
-    ;
+    return  <ul className="nav navbar-nav navbar-right">
+              <li><a href="/login">Login</a></li>
+              <li><a href="/register">Register</a></li>
+            </ul>;
   },
   render() {
     return (
