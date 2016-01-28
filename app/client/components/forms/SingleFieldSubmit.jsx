082c8b464c23a02e3424c04af4a64056f6358006
Anders Ramsay <andersr@gmail.com>
2016-01-28 13:46:28 -0500

    add new task

---
+++ app/client/components/forms/SingleFieldSubmit.jsx
@@ -0,0 +1,40 @@
+SingleFieldSubmit = React.createClass({
+  propTypes: {
+    handleInput: React.PropTypes.func.isRequired
+  },
+  getDefaultProps() {
+    return {
+      inputValue:  "",
+      placeholder: "New..."
+    };
+  },
+  getInitialState() {
+    return {
+      inputValue: this.props.inputValue
+    };
+  },
+  updateInputValue(e){
+  	this.setState({inputValue: e.target.value});
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleInput(this.state.inputValue.trim());
+    this.setState({ inputValue: "" });
+  },
+
+  render() {
+    return (
+      <form className="form-inline" onSubmit={this.handleSubmit}>
+       <div className="form-group">
+         <input
+          className="form-control"
+          type="text"
+          placeholder={this.props.placeholder}
+          value={this.state.inputValue}
+          onChange={this.updateInputValue}
+        />
+        </div>
+      </form>
+    )
+  }
+});
--- app/client/components/views/TasksList.jsx
+++ app/client/components/views/TasksList.jsx
@@ -11,16 +11,32 @@ TasksList = React.createClass({
       tasks: Tasks.find({}, {sort: { createdAt: -1 }}).fetch()
     }
   },
+  handleInsertTask(inputValue) {
+  	let taskAttributes = {
+      title: inputValue,
+      createdAt: new Date(),
+      done: false,
+      ownerId: Meteor.userId()
+    };
+    Tasks.insert(taskAttributes);
+
+	   // Meteor.call('/task/insert', inputValue, function(err, result){
+	   // 	 if (err) { console.log('there was an error: ' + err.reason); };
+    // });
+  },
   render() {
     return (
         <div className="row">
           <div className="col-md-6 col-md-offset-3">
            <h1>My Tasks</h1>
-              <ul className="list-group">
-                  <li className="list-group-item editable">
-                  Some Task
-                  </li>
-              </ul>
+    <ul className="list-group">
+        <li className="list-group-item editable">
+            <SingleFieldSubmit
+              placeholder="New task..."
+              handleInput={this.handleInsertTask}
+             />
+        </li>
+    </ul>
           </div>
         </div>
       );
