437a0aebb857f371108f1877fcab791426722bff
Anders Ramsay <andersr@gmail.com>
2016-01-30 12:42:17 -0500

    add done field and methdods

--- app/both/collections/Tasks.js
+++ app/both/collections/Tasks.js
@@ -7,9 +7,19 @@ Meteor.methods({
     let taskAttributes = {
       title: title,
       createdAt: new Date(),
+      done: false,
       ownerId: Meteor.userId()
     };
     Tasks.insert(taskAttributes);
+  },
+
+  '/task/update/done': function(id) {
+    let task = Tasks.findOne({_id: id });
+    Tasks.update(task._id, {
+      $set: {
+        done: !task.done
+       }
+    });
   }
 
 });
